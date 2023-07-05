b8de08e601c290094c5ff6486f4d7f9b460309a3
Dilshad <dlz611606@gmail.com>
2023-07-05 10:51:21 +0530

    cc

--- core/views.py
+++ core/views.py
@@ -5,7 +5,7 @@ from django.contrib import messages
 from .models import Profile, Post, LikePost, FollowersCount
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect
-
+#411
 
 @login_required(login_url='login')
 def index(request):
